3ec8f29ffcd0548cd555031f2048573b6c566308
Mihai Popescu <mihai-radu-niculae.popescu11@alumni.imperial.ac.uk>
2020-10-11 19:08:42 +0300

    SYSV MQ: Writer configuration parameters

--- src/sysv-mq/blocking_writer.c
+++ src/sysv-mq/blocking_writer.c
@@ -1,10 +1,12 @@
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #ifndef DEFAULT_BUF_LEN
 #define DEFAULT_BUF_LEN (8192 - sizeof(long))
@@ -14,7 +16,7 @@
  * Doesn't matter what the path is as long as it is "accessible"
  */
 #ifndef DEFAULT_QPATH
-#define DEFAULT_QPATH "./";
+#define DEFAULT_QPATH "./"
 #endif
 
 /*
@@ -42,32 +44,82 @@ struct msg {
     char msg_text[DEFAULT_BUF_LEN];
 };
 
-char info_str[1 << 10];
-struct msg send_msg_buffer;
+static char info_str[1 << 10];
+static struct msg send_msg_buffer;
 
 /*
  * It should fit in both DEFAULT_BUF_LEN and the system MSGMAX.
  * Implementations differ in their support for determining
- * these values. Linux offers a configurable value in
- * "/proc/sys/kernel/msgmax"
+ * these values. Linux offers a configurable value at
+ * /proc/sys/kernel/msgmax
  */
 size_t get_msg_max() {
     return DEFAULT_BUF_LEN;
 }
 
-int main(int argc, char *argv[]) {
-    char * const qpath = DEFAULT_QPATH;
-    int const proj_id = DEFAULT_PROJ_ID;
-
+struct writer_cfg {
+    char * qpath;
+    int proj_id;
     /*
-     * This is what we'll use for splitting input
+     * This field below is what we'll use for splitting input
      */ 
-    size_t const msg_max = get_msg_max();
-    
-    key_t qkey = ftok(qpath, proj_id);
+    size_t msg_max;
+    bool blocking;
+};
+
+struct writer_cfg get_writer_cfg(int argc, char *argv[]) {
+    struct writer_cfg cfg = {
+        .qpath = DEFAULT_QPATH,
+        .proj_id = DEFAULT_PROJ_ID,
+        .msg_max = get_msg_max(),
+        .blocking = true
+    };
+
+    int opt;
+
+    while ((opt = getopt(argc, argv, "p:i:n:Wh")) != -1) {
+        switch (opt) {
+            case 'p': {
+                          cfg.qpath = optarg;
+
+                          break;
+                      }
+            case 'i': {
+                          cfg.proj_id = atoi(optarg);
+
+                          break;
+                      }
+            case 'n': {
+                          cfg.msg_max = atoi(optarg);
+
+                          break;
+                      }
+            case 'W': {
+                          cfg.blocking = false;
+                      }
+            case 'h': {
+                          fprintf(stderr, "TODO: write this help\n");
+
+                          // TODO: improve this exit
+                          exit(EXIT_SUCCESS);
+                      }
+            default: /* '?' */
+                fprintf(stderr, "TODO: write the proper usage\n", argv[0]);
+
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    return cfg;
+}
+
+int main(int argc, char *argv[]) {
+    struct writer_cfg cfg = get_writer_cfg(argc, argv);
+
+    key_t qkey = ftok(cfg.qpath, cfg.proj_id);
 
     if (qkey < 0) {
-        sprintf(info_str, "Could not get key for mq %s:%d", qpath, proj_id);
+        sprintf(info_str, "Could not get key for mq %s:%d", cfg.qpath, cfg.proj_id);
         perror(info_str);
 
         goto err;
@@ -88,8 +140,8 @@ int main(int argc, char *argv[]) {
 
     size_t msg_bytes;
 
-    while (msg_bytes = fread(send_msg_buffer.msg_text, 1, msg_max, stdin)) {
-        if (msg_bytes < msg_max && ferror(stdin) != 0) {
+    while (msg_bytes = fread(send_msg_buffer.msg_text, 1, cfg.msg_max, stdin)) {
+        if (msg_bytes < cfg.msg_max && ferror(stdin) != 0) {
             sprintf(info_str, "Bad read from stdin");
             perror(info_str);
 
@@ -98,7 +150,8 @@ int main(int argc, char *argv[]) {
 
         send_msg_buffer.msg_type = MSG_CHUNK;
 
-        int send_result = msgsnd(qid, (void *)&send_msg_buffer, msg_bytes, 0);
+        int send_flgs = cfg.blocking ? 0 : IPC_NOWAIT;
+        int send_result = msgsnd(qid, (void *)&send_msg_buffer, msg_bytes, send_flgs);
 
         if (send_result < 0) {
             sprintf(info_str,"Could not send message chunk on queue");
@@ -106,14 +159,16 @@ int main(int argc, char *argv[]) {
 
             goto err;
         }
+
+        fprintf(stderr, "Sent message chunk: %zu bytes\n", msg_bytes);
     }
 
-    fprintf(stderr, "Sent message chunk\n");
 
-    return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
 
 err:
     fprintf(stderr, "Program encountered errors, exiting\n");
 
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
 }
+
